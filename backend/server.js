5d6bddb965bd861cf5cd0d9422225532d009f482
BinjuRai <binjurai7@gmail.com>
2026-01-29 02:13:15 +0545

    some server changes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,8 @@
-require("dotenv").config()
+require("dotenv").config();
 const { server } = require("./index");
 
-const PORT = process.env.PORT || 5050
+const PORT = process.env.PORT || 5050;
 
-// server.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
-server.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on http://0.0.0.0:${PORT}`);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
